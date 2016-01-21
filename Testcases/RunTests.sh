04b52e3d980846288da476e40ef1cae26ae2d1f0
Stuart Mackie <wsmackie@juniper.net>
2016-01-21 12:36:48 -0500

    Add python helper scripts
    Change-Id: I83b378941082d8f9aeae88f6cf9e586d8eb7458d
    Signed-off-by: Stuart Mackie <wsmackie@juniper.net>

--- Testcases/RunTests.sh
+++ Testcases/RunTests.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
 PATH=${OVNO_COMMIT}/Testcases:$PATH
+cd ${OVNO_COMMIT}/Testcases
+
+# Create the config wrapper
+OCL_IP=`echo $OS_AUTH_URL | cut -d "/" -f3 | cut -d ":" -f1`
+
+cat <<EOF >config
+#!/usr/bin/python
+
+import sys
+import os
+from config_shell import *
+default_client_args = [
+    ('--username', 'admin'),
+    ('--password', os.environ["OS_PASSWORD"]),
+    ('--region', 'RegionOne'),
+    ('--tenant', 'admin'),
+    ('--api-server', os.environ["OCL_IP"])]
+
+
+if __name__ == '__main__':
+    for arg in default_client_args:
+        if not arg[0] in sys.argv:
+            sys.argv.insert(1, arg[0])
+            sys.argv.insert(2, arg[1])
+    ConfigShell().main()
+EOF
+
+chmod 777 config
+
+
+
 
 echo "Starting OpenContrail test suite"
 # Tests go here
